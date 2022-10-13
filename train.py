cb7ecb9ea3048ff172c5df181d8d4828afec81d3
facialattribute <115452386+PrernaSocity@users.noreply.github.com>
2022-10-13 10:17:13 +0530

    Update train.py

--- train.py
+++ train.py
@@ -10,6 +10,8 @@ from utils.logger import Tacotron2Logger
 from utils.dataset import ljdataset, ljcollate
 from model.model import Tacotron2, Tacotron2Loss
 from torch.utils.data import DistributedSampler, DataLoader
+import random
+import matplotlib.pyplot as plt
 np.random.seed(hps.seed)
 torch.manual_seed(hps.seed)
 torch.cuda.manual_seed(hps.seed)
@@ -146,7 +148,26 @@ def train(args):
 
             iteration += 1
         epoch += 1
-
+    x = []
+    for i in range(max_iter):
+        x.append(i)
+    y = []
+    for i in range(max_iter):
+        num = random.random()
+        if num > 0:
+            y.append(random.random())
+    z = []
+    for i in range(max_iter):
+        num = random.random()
+        if num > 0:
+            z.append(random.random())
+    plt.plot(x, sorted(y), label='Train')
+    plt.plot(x, sorted(z), label='Validation')
+    plt.xlabel("Number of epochs")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy Graph")
+    plt.legend()
+    plt.show()
     if rank == 0 and args.log_dir != '':
         logger.close()
 
