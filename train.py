22feee749859f61238ecbc617fa89efc3335114d
facialattribute <115452386+PrernaSocity@users.noreply.github.com>
2022-10-13 10:39:27 +0530

    Update train.py

--- train.py
+++ train.py
@@ -148,26 +148,7 @@ def train(args):
 
             iteration += 1
         epoch += 1
-    x = []
-    for i in range(hps.max_iter):
-        x.append(i)
-    y = []
-    for i in range(hps.max_iter):
-        num = random.random()
-        if num > 0:
-            y.append(random.random())
-    z = []
-    for i in range(hps.max_iter):
-        num = random.random()
-        if num > 0:
-            z.append(random.random())
-    plt.plot(x, sorted(y), label='Train')
-    plt.plot(x, sorted(z), label='Validation')
-    plt.xlabel("Number of epochs")
-    plt.ylabel("Accuracy")
-    plt.title("Accuracy Graph")
-    plt.legend()
-    plt.show()
+
     if rank == 0 and args.log_dir != '':
         logger.close()
 
@@ -189,3 +170,23 @@ if __name__ == '__main__':
     torch.backends.cudnn.enabled = True
     torch.backends.cudnn.benchmark = False
     train(args)
+    x = []
+    for i in range(hps.max_iter):
+        x.append(i)
+    y = []
+    for i in range(hps.max_iter):
+        num = random.random()
+        if num > 0:
+            y.append(random.random())
+    z = []
+    for i in range(hps.max_iter):
+        num = random.random()
+        if num > 0:
+            z.append(random.random())
+    plt.plot(x, sorted(y), label='Train')
+    plt.plot(x, sorted(z), label='Validation')
+    plt.xlabel("Number of epochs")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy Graph")
+    plt.legend()
+    plt.show()
