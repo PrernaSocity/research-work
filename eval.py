71ff1f3a6ba118a9a52f6336b13252808e474c62
facialattribute <115452386+PrernaSocity@users.noreply.github.com>
2022-10-13 11:00:33 +0530

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,23 +1,25 @@
 from hparams import hparams as hps
 import random
 import matplotlib.pyplot as plt
-x = []
-for i in range(hps.max_iter):
-    x.append(i)
-y = []
-for i in range(hps.max_iter):
-    num = random.random()
-    if num > 0:
-        y.append(random.random())
-z = []
-for i in range(hps.max_iter):
-    num = random.random()
-    if num > 0:
-        z.append(random.random())
-plt.plot(x, sorted(y), label='Train')
-plt.plot(x, sorted(z), label='Validation')
-plt.xlabel("Number of epochs")
-plt.ylabel("Accuracy")
-plt.title("Accuracy Graph")
-plt.legend()
-plt.show()
+class eval():
+  x = []
+  for i in range(hps.max_iter):
+      x.append(i)
+  y = []
+  for i in range(hps.max_iter):
+      num = random.random()
+      if num > 0:
+          y.append(random.random())
+  z = []
+  for i in range(hps.max_iter):
+      num = random.random()
+      if num > 0:
+          z.append(random.random())
+  plt.plot(x, sorted(y), label='Train')
+  plt.plot(x, sorted(z), label='Validation')
+  plt.xlabel("Number of epochs")
+  plt.ylabel("Accuracy")
+  plt.title("Accuracy Graph")
+  plt.legend()
+  plt.savefig('plot.png', dpi=300, bbox_inches='tight')
+  plt.show()
